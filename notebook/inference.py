88dc56d7797865d31357e498419c249fca86c1d2
Reza Dadashian <rezadadashian@yahoo.ca>
2026-01-24 14:06:56 +0330

    Disable layout postprocessing in Inference class for improved performance

--- notebook/inference.py
+++ notebook/inference.py
@@ -114,7 +114,7 @@ class Inference:
             stage1_only=False,
             with_mesh_postprocess=True,
             with_texture_baking=True,
-            with_layout_postprocess=True,
+            with_layout_postprocess=False,
             use_vertex_color=False,
             stage1_inference_steps=None,
             pointmap=pointmap,
