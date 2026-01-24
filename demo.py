312bba249d57bc97edb919912991b42c776dd927
Reza Dadashian <rezadadashian@yahoo.ca>
2026-01-24 13:54:03 +0330

    Update mask loading and change output file format to GLB

--- demo.py
+++ demo.py
@@ -12,11 +12,13 @@ inference = Inference(config_path, compile=False)
 
 # load image (RGBA only, mask is embedded in the alpha channel)
 image = load_image("notebook/images/shutterstock_stylish_kidsroom_1640806567/image.png")
-mask = load_single_mask("notebook/images/shutterstock_stylish_kidsroom_1640806567", index=14)
+mask = load_single_mask(
+    "notebook/images/shutterstock_stylish_kidsroom_1640806567", index=14
+)
 
 # run model
 output = inference(image, mask, seed=42)
 
 # export gaussian splat
-output["gs"].save_ply(f"splat.ply")
-print("Your reconstruction has been saved to splat.ply")
+output["glb"].save_ply(f"splat.glb")
+print("Your reconstruction has been saved to splat.glb")
