efe220cf46d0483e1c3d4b44a787a205582195a5
Reza Dadashian <rezadadashian@yahoo.ca>
2026-02-16 08:57:24 +0330

    Add demo script for 3D object reconstruction using inference model

---
+++ demo-ply.py
@@ -0,0 +1,27 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+import sys
+
+# import inference code
+sys.path.append("notebook")
+from inference import Inference, load_image, load_mask, load_single_mask
+
+# load model
+tag = "hf"
+config_path = f"checkpoints/{tag}/pipeline.yaml"
+inference = Inference(config_path, compile=False)
+
+# load image (RGBA only, mask is embedded in the alpha channel)
+image = load_image("notebook/images/sofa/sofa.jpeg")
+mask = load_single_mask("notebook/images/sofa", index=1)
+
+# run model
+output = inference(
+    image,
+    mask,
+    seed=1,
+    rendering_engine="pytorch3d",
+)
+
+# export gaussian splat
+output["gs"].save_ply(f"splat.ply")
+print("Your reconstruction has been saved to splat.glb")
